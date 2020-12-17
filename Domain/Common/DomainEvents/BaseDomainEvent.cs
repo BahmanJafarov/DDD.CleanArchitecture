e4238f4a3ddd1866344de9d2004b592d7a61653e
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2020-12-17 21:57:04 +0400

    Add project files.

---
+++ Domain/Common/DomainEvents/BaseDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+
+namespace AppDomain.Common.DomainEvents
+{
+    public abstract class BaseDomainEvent
+    {
+        public DateTime DateOccured { get; protected set; } = DateTime.UtcNow;
+    }
+}
---
+++ Domain/Common/ValueObjects/IgnoreMemberAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AppDomain.Common.ValueObjects
+{
+    // source: https://github.com/jhewlett/ValueObject
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
