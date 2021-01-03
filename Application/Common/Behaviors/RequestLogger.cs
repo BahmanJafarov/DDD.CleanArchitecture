863ddc36b700c5c2454d1aa39fe37d34e52a97e1
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-01-03 21:58:51 +0400

    .

---
+++ Application/Common/Behaviors/RequestLogger.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Behaviors
+{
+    public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger<TRequest> _logger;
+        private readonly ICurrentUserService _currentUserService;
+
+        public RequestLogger(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+        {
+            _logger = logger;
+            _currentUserService = currentUserService;
+        }
+
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId;
+
+            _logger.LogInformation("Request: {Name}, UserId {@UserId}, {@Request}", requestName, userId, request);
+
+            await Task.CompletedTask;
+        }
+    }
+}
