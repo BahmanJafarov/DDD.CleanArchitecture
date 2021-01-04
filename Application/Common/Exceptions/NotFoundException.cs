207417b0b6518ef6f307fbb3d61024cfccf249d7
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-01-04 22:53:58 +0400

    .

---
+++ Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+
+        }
+    }
+}
---
+++ Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Application.Tasks.Queries.GetTasks;
+using AutoMapper;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType && (i.GetGenericTypeDefinition() == typeof(IMapFrom<>) || i.GetGenericTypeDefinition() == typeof(IMapTo<>))))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+
+
+
+                var methodInfo = type.GetMethod("Mapping", BindingFlags.Instance | BindingFlags.NonPublic)
+                                 ?? (type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>))
+                                     ? type.GetInterface("IMapFrom`1").GetMethod("Mapping")
+                                     : type.GetInterface("IMapTo`1").GetMethod("Mapping"));
+
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
