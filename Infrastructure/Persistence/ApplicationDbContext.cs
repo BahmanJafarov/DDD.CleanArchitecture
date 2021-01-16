b3893e58b981295286982bf88e37d5a8f8d7f03b
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-01-17 00:43:36 +0400

    .

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using AppDomain.Entities;
+﻿using AppDomain.Common.DomainEvents;
+using AppDomain.Common.Entities;
+using AppDomain.Entities;
 using Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence
@@ -21,6 +24,52 @@ namespace Infrastructure.Persistence
         {
             _currentUserService = currentUserService;
             _mediator = mediator;
+        }
+
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<IAudited>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        SetCreationAuditedProperties(entry);
+                        break;
+                    case EntityState.Modified:
+                        SetModificationAuditedProperties(entry);
+                        break;
+                    case EntityState.Deleted:
+                        CancelDeletionForSoftDelete(entry);
+                        SetDeletionAuditedProperties(entry);
+                        break;
+                }
+            }
+
+            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            if (_mediator == null)
+            {
+                return result;
+            }
+
+            var entitiesWithEvents = ChangeTracker.Entries<HaveDomainEvents>()
+                .Select(e => e.Entity)
+                .Where(e => e.Events.Any())
+                .ToArray();
+
+            foreach (var entity in entitiesWithEvents)
+            {
+                var events = entity.Events.ToArray();
+                entity.Events.Clear();
+
+                foreach (var domainEvent in events)
+                {
+                    await _mediator.Publish(domainEvent, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            return result;
         }
 
 
---
+++ Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+
+
+        public DateTime UtcNow()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
